c3034f6ff074d3d60366aad16a35897fea8fc9df
cchuan <caizhengchuan@163.com>
2017-12-21 18:20:11 +0800

    添加测试文件

---
+++ CCToolsManager/CCToolsManagerDemo/CCToolsManager/TestFile.h
@@ -0,0 +1,13 @@
+//
+//  TestFile.h
+//  CCToolsManagerDemo
+//
+//  Created by cchuan on 2017/12/21.
+//  Copyright © 2017年 cai chuan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestFile : NSObject
+
+@end
---
+++ CCToolsManager/CCToolsManagerDemo/CCToolsManager/TestFile.m
@@ -0,0 +1,13 @@
+//
+//  TestFile.m
+//  CCToolsManagerDemo
+//
+//  Created by cchuan on 2017/12/21.
+//  Copyright © 2017年 cai chuan. All rights reserved.
+//
+
+#import "TestFile.h"
+
+@implementation TestFile
+
+@end
